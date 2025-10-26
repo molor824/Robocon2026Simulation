01685fa724680282596360bb812bc2b53d21aa46
molor824 <molormargaderdene@gmail.com>
2025-10-26 19:47:07 +0800

    First commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Vector2 _rotation;
+
+    [SerializeField]
+    private float _speed = 10.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
